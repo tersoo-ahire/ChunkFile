00edb1aa57f325cf123523550fd9c58e01b41b64
Tersoo <tersooahire21@gmail.com>
2022-07-25 12:13:30 +0100

    made changes

--- chunkfile/splitfile/admin.py
+++ chunkfile/splitfile/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from splitfile.models import User, CSVFile, JSONFile
+from splitfile.models import CSVFile, JSONFile
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(CSVFile)
 admin.site.register(JSONFile)
